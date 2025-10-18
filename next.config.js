d93a7634bb3c7cea8f4a13139152b737a3136a5f
SM ZOBAYER <smzobayer@gmail.com>
2025-10-18 00:40:00 -0500

    Disable TypeScript checking during builds

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     eslint: {
       ignoreDuringBuilds: true,
     },
+    typescript: {
+      ignoreBuildErrors: true,  // ADD THIS LINE
+    },
   };
   
   module.exports = nextConfig;
